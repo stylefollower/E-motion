f147e8630e24943eb188a8e474cc54976b209680
perrarian <gonzalo.smb1@gmail.com>
2021-09-28 16:19:44 +0200

    new commit

--- storage/framework/views/b18d705f49d4cb87cb2bde363b3ca5bd2bbb5823.php
+++ storage/framework/views/b18d705f49d4cb87cb2bde363b3ca5bd2bbb5823.php
@@ -8,7 +8,7 @@
     <div class="container">
       
       <img class="img-fluid" src="<?php echo e(URL::asset('/image/car1.jpg')); ?>" alt="main site image">
-      <h1 class="jumbotron-heading my-2">Post Example</h1>
+      <h1 class="jumbotron-heading my-3">Post Example</h1>
       <p class="lead text-muted">Something short and leading about the collection below—its contents, the creator, etc. Make it short and sweet, but not too short so folks don't simply skip over it entirely.</p>
       <p>
           
@@ -16,6 +16,7 @@
 
       </p>
     </div>
+    
   </section>
 
   
@@ -79,8 +80,6 @@
         <div class="col-md-4">
           <div class="card mb-4 box-shadow">
             <img class="card-img-top" src="<?php echo e(URL::asset('/image/art1.png')); ?>" alt="Card image cap">
-
-            <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&bg=55595c&fg=eceeef&text=Thumbnail" alt="Card image cap">
             <div class="card-body">
               <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
               <div class="d-flex justify-content-between align-items-center">
@@ -96,8 +95,6 @@
         <div class="col-md-4">
           <div class="card mb-4 box-shadow">
             <img class="card-img-top" src="<?php echo e(URL::asset('/image/art2.png')); ?>" alt="Card image cap">
-
-            <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&bg=55595c&fg=eceeef&text=Thumbnail" alt="Card image cap">
             <div class="card-body">
               <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
               <div class="d-flex justify-content-between align-items-center">
@@ -113,8 +110,6 @@
         <div class="col-md-4">
           <div class="card mb-4 box-shadow">
             <img class="card-img-top" src="<?php echo e(URL::asset('/image/art3.png')); ?>" alt="Card image cap">
-
-            <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&bg=55595c&fg=eceeef&text=Thumbnail" alt="Card image cap">
             <div class="card-body">
               <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
               <div class="d-flex justify-content-between align-items-center">
@@ -176,8 +171,6 @@
         <div class="col-md-4">
           <div class="card mb-4 box-shadow">
             <img class="card-img-top" src="<?php echo e(URL::asset('/image/art1.png')); ?>" alt="Card image cap">
-
-            <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&bg=55595c&fg=eceeef&text=Thumbnail" alt="Card image cap">
             <div class="card-body">
               <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
               <div class="d-flex justify-content-between align-items-center">
@@ -192,11 +185,7 @@
         </div>
         <div class="col-md-4">
           <div class="card mb-4 box-shadow">
-
             <img class="card-img-top" src="<?php echo e(URL::asset('/image/art2.png')); ?>" alt="Card image cap">
-
-            <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&bg=55595c&fg=eceeef&text=Thumbnail" alt="Card image cap">
-
             <div class="card-body">
               <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
               <div class="d-flex justify-content-between align-items-center">
@@ -211,11 +200,7 @@
         </div>
         <div class="col-md-4">
           <div class="card mb-4 box-shadow">
-
             <img class="card-img-top" src="<?php echo e(URL::asset('/image/art3.png')); ?>" alt="Card image cap">
-
-            <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&bg=55595c&fg=eceeef&text=Thumbnail" alt="Card image cap">
-
             <div class="card-body">
               <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
               <div class="d-flex justify-content-between align-items-center">
@@ -234,84 +219,7 @@
 
 </main>
 
-      
-          
-<footer>
-  <div class="container">
-    <div class="row">
-
-      <div class="col-lg-4 col-md-6">
-        <h3>Site Map</h3>
-        <ul class="list-unstyled three-column">
-          <li>Home</li>
-          <li>Services</li>
-          <li>About</li>
-          <li>Code</li>
-          <li>Design</li>
-          <li>Host</li>
-          <li>Contact</li>
-          <li>Company</li>
-        </ul>
-        <ul class="list-unstyled socila-list">
-          <li><img src="http://placehold.it/48x48" alt="" /></li>
-          <li><img src="http://placehold.it/48x48" alt="" /></li>
-          <li><img src="http://placehold.it/48x48" alt="" /></li>
-          <li><img src="http://placehold.it/48x48" alt="" /></li>
-          <li><img src="http://placehold.it/48x48" alt="" /></li>
-          <li><img src="http://placehold.it/48x48" alt="" /></li>
-        </ul>
-    </div>
-
-      <div class="col-lg-4 col-md-6">
-        <h3>latest Articles</h3>
-        <div class="media">
-          <a href="#" class="pull-left">
-            <img src="http://placehold.it/64x64" alt="" class="media-object" />
-          </a>
-          <div class="media-body">
-            <h4 class="media-heading">Programming</h4>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-          </div>
-        </div>
-
-        <div class="media">
-          <a href="#" class="pull-left">
-            <img src="http://placehold.it/64x64" alt="" class="media-object" />
-          </a>
-          <div class="media-body">
-            <h4 class="media-heading">Coding</h4>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-          </div>
-        </div>
-
-        
-          </div>
-      
-      <div class="col-lg-4">
-        <h3>Our Work</h3>
-        <img class="img-thumbnail" src="http://placehold.it/150x100" alt="" />
-        <img class="img-thumbnail" src="http://placehold.it/150x100" alt="" />
-        </div>
-      
-      </div>
-    </div>
-  <div class="copyright text-center">
-    Copyright &copy; 2017 <span>Your Template Name</span>
-  </div>
-</footer>
+<?php $__env->stopSection(); ?>  
 
 
-</main>
-
-<!-- Bootstrap core JavaScript
-================================================== -->
-<!-- Placed at the end of the document so the pages load faster -->
-
-
-      <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-      <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
-      <script src="../../assets/js/vendor/popper.min.js"></script>
-      <script src="../../dist/js/bootstrap.min.js"></script>
-      <script src="../../assets/js/vendor/holder.min.js"></script>
-</html>
-<?php /**PATH /var/www/html/resources/views/welcome.blade.php ENDPATH**/ ?>
+<?php echo $__env->make('layouts.nav', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH /var/www/html/resources/views/welcome.blade.php ENDPATH**/ ?>
