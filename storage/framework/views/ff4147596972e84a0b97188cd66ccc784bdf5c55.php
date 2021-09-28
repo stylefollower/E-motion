18a169fd9bc17dea78acddf222e3b60630841956
Marcopolo18 <82804068+Marcopolo18@users.noreply.github.com>
2021-09-28 09:50:36 +0200

    commentoutstyle

--- storage/framework/views/ff4147596972e84a0b97188cd66ccc784bdf5c55.php
+++ storage/framework/views/ff4147596972e84a0b97188cd66ccc784bdf5c55.php
@@ -15,79 +15,59 @@
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-    <link rel="stylesheet" href="https://bootswatch.com/5/lux/bootstrap.min.css">
-    
+    <link rel="stylesheet" href="https://bootswatch.com/5/cerulean/bootstrap.min.css">
 
     <!-- Styles -->
     <link href="<?php echo e(asset('css/app.css')); ?>" rel="stylesheet">
-</head> 
-<body class="antialiased">
-  <div id="app">
-    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-      <div class="container-fluid">
-        <a class="navbar-brand" href="<?php echo e(url('/')); ?>">
-           <?php echo e(config('app.name', 'E-Motion')); ?>
+</head>
+<body>
+    <div id="app">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+            <div class="container-fluid">
+              <a class="navbar-brand" href="#">Navbar</a>
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+          
+              <div class="collapse navbar-collapse" id="navbarColor01">
+                <ul class="navbar-nav me-auto">
+                  <li class="nav-item active">
+                    <a class="nav-link" href="#">Home
+                      <span class="visually-hidden">(current)</span>
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">Features</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">Pricing</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">About</a>
+                  </li>
+                  <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+                    <div class="dropdown-menu">
+                      <a class="dropdown-item" href="#">Action</a>
+                      <a class="dropdown-item" href="#">Another action</a>
+                      <a class="dropdown-item" href="#">Something else here</a>
+                      <div class="dropdown-divider"></div>
+                      <a class="dropdown-item" href="#">Separated link</a>
+                    </div>
+                  </li>
+                </ul>
+                <form class="d-flex">
+                  <input class="form-control me-sm-2" type="text" placeholder="Search">
+                  <button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
+                </form>
+              </div>
+            </div>
+          </nav>
 
-        </a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="<?php echo e(__('Toggle navigation')); ?>">
-     <span class="navbar-toggler-icon"></span>
-     </button>
-    
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <!-- Left Side Of Navbar -->
-    <ul class="navbar-nav mr-auto">
-    
-    </ul>
-    
-    <!-- Right Side Of Navbar -->
-    <ul class="navbar-nav ml-auto">
-    <!-- Authentication Links -->
-    <?php if(auth()->guard()->guest()): ?>
-    <?php if(Route::has('login')): ?>
-    <li class="nav-item">
-    <a class="nav-link" href="<?php echo e(route('login')); ?>"><?php echo e(__('Login')); ?></a>
-    </li>
-    <?php endif; ?>
-    
-    <?php if(Route::has('register')): ?>
-    <li class="nav-item">
-    <a class="nav-link" href="<?php echo e(route('register')); ?>"><?php echo e(__('Register')); ?></a>
-    </li>
-    <?php endif; ?>
-    <?php else: ?>
-    <li class="nav-item dropdown">
-    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-    <?php echo e(Auth::user()->name); ?>
-
-    </a>
-    
-    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-    <a class="dropdown-item" href="<?php echo e(route('logout')); ?>"
-    onclick="event.preventDefault();
-    document.getElementById('logout-form').submit();">
-    <?php echo e(__('Logout')); ?>
-
-    </a>
-    
-    <form id="logout-form" action="<?php echo e(route('logout')); ?>" method="POST" class="d-none">
-    <?php echo csrf_field(); ?>
-    </form>
+        <main class="py-4">
+            <?php echo $__env->yieldContent('content'); ?>
+        </main>
     </div>
-    </li>
-    <?php endif; ?>
-    </ul>
-    </div>
-    </div>
-    </nav>
-    
-    <main class="py-4">
-    <?php echo $__env->yieldContent('content'); ?>
-    </main>
-    </div>
-    </div>
-
-    
-
 </body>
 </html>
 <?php /**PATH /var/www/html/resources/views/layouts/app.blade.php ENDPATH**/ ?>
